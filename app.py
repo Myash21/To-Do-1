b3580570cb4572517f532e2306b39dd0fdb55142
Yash Mhaskar <myash100@gmail.com>
2024-11-08 23:43:00 -0800

    added database

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(app)
 
-@app.route('/', methods = ["GET"])
+@app.route('/', methods=["GET"])
 def hello():
     return render_template("index.html")
 
+@app.route('/add', methods = ['POST'])
+def add():
+    pass
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    with app.app_context():  # Push the application context
+        db.create_all()       # Create tables in the database
+    app.run(debug=True)
