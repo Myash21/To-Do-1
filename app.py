078cf34ee7374fc439c9d93729c3fb437dafbbdc
Yash Mhaskar <myash100@gmail.com>
2024-11-09 00:59:34 -0800

    completed all operations

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,19 +6,40 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
 @app.route('/', methods=["GET"])
 def hello():
     todo_list = Todo.query.all()
-    return render_template("index.html", todo_list = todo_list)
+    return render_template("index.html", todo_list=todo_list)
+
+@app.route('/add', methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("hello"))
+
+@app.route('/update/<int:todo_id>')
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()  # Retrieve the Todo instance
+    if todo:  # Check if todo exists
+        todo.complete = not todo.complete
+        db.session.commit()
+    return redirect(url_for("hello"))
+
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()  # Retrieve the Todo instance
+    if todo:  # Check if todo exists
+        db.session.delete(todo)
+        db.session.commit()
+    return redirect(url_for("hello"))
 
 if __name__ == "__main__":
-    with app.app_context():  # Push the application context
-        db.create_all()       # Create tables in the database
-        new_todo = Todo(title = 'todo 1', complete = False)
-        db.session.add(new_todo)
-        db.session.commit()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
